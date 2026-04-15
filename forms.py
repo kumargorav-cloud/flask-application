0d8439db4bf421220c2a770b8fe68ad9a6295ef0
kumargorav-cloud <singhalanhilom145@gmail.com>
2026-04-15 12:49:30 +0000

    new commit

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,Response,send_from_directory
 import pandas as pd
+import uuid
 # in this we will learn about handling forms data
+import os
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
@@ -26,6 +28,53 @@ def file_upload():
     elif file.content_type =="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file.content_type == "application/vnd.ms-excel":
         df = pd.read_excel(file)
         return df.to_html()
+
+
+@app.route("/convert_to_csv",methods=['POST'])
+def convert_to_csv():
+    file = request.files['file']
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition':'attachment; filename=result.csv'
+        }
+    )
+    return response
+
+@app.route("/convert_csv_two",methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+    df = pd.read_excel(file)
+
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+
+    filename = f"{uuid.uuid4()}.csv"
+    
+    df.to_csv(os.path.join('downloads',filename))
+              
+    return render_template('downloads.html',filename=filename)
+
+
+@app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory('downloads',filename,download_name='result.csv')
+
+
+@app.route("/handle_request", methods=['POST'])
+def handle_request():
+    gretting = request.json['gretting']
+    name = request.json['name']
+
+    with open('file.txt','w') as f:
+        f.write(f"{gretting},{name}")
+
+    return {'message':'success'}
+
+
 
 
 if __name__ == "__main__":
